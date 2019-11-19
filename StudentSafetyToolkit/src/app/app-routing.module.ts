1a2c8c8428a0b6e6d4412cacf4b5312b5692ac92
Nikita Chernenky <chernenm@uregina.ca>
2019-11-19 11:38:33 -0600

    deleted default home page as I won't use it

--- StudentSafetyToolkit/src/app/app-routing.module.ts
+++ StudentSafetyToolkit/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'app-info', pathMatch: 'full' },
   {
     path: 'contacts',
     loadChildren: () => import('./contacts/contacts.module').then( m => m.ContactsPageModule)
