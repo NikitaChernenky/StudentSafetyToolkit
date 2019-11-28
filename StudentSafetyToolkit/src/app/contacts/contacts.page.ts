c35d65a6d6c3c306517a54a4862839dfd9e2ca7c
Nikita Chernenky <chernenm@uregina.ca>
2019-11-28 10:41:16 -0600

    contacts page

--- StudentSafetyToolkit/src/app/contacts/contacts.page.ts
+++ StudentSafetyToolkit/src/app/contacts/contacts.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from 'src/app/interfaces/metadata';
+import { Router } from '@angular/router';
+import { MetadataService } from 'src/app/services/metadata.service';
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.page.scss'],
 })
 export class ContactsPage implements OnInit {
-
-  constructor() { }
+  contacts: Contact[];
+  constructor(private dataService: MetadataService, private router: Router) { }
 
   ngOnInit() {
+    this.getContacts();
   }
+
+
+  getContacts(): void {
+    this.dataService.getContactsData()
+      .subscribe(contacts => this.contacts = contacts);
+  }
+
+  continueToEmail() {
+    this.router.navigate(['/email']);
+  }
+
 
 }
