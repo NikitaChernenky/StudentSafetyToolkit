b719497ae253e40a3c37561b549af8496b26a8b8
Nikita Chernenky <chernenm@uregina.ca>
2019-11-28 08:52:55 -0600

    disclaimer page ready

--- StudentSafetyToolkit/src/app/disclaimer/disclaimer.module.ts
+++ StudentSafetyToolkit/src/app/disclaimer/disclaimer.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
 import { DisclaimerPageRoutingModule } from './disclaimer-routing.module';
-
 import { DisclaimerPage } from './disclaimer.page';
 
 @NgModule({
